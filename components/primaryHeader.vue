0eb3684c32fa16794bfb4f42e829d19029ff4868
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-09-10 08:57:09 +0400

    primary header aos

--- components/primaryHeader.vue
+++ components/primaryHeader.vue
@@ -1,10 +1,10 @@
 <template>
   <header class="w-full sticky top-0 bg-black z-10">
     <div class="container max-w-8xl mx-auto flex justify-between py-3 shadow-2xl">
-      <div class="flex items-center">
+      <div class="flex items-center" data-aos="zoom-in" data-aos-once="true">
         <img class="pointer-events-none select-none" src="/img/logo.png" alt="Drive">
       </div>
-      <nav class="flex gap-5 uppercase text-white items-center font-sans text-sm font-light">
+      <nav data-aos="fade-in" data-aos-once="true" class="hidden md:flex gap-5 uppercase text-white items-center font-sans text-sm font-light">
         <a href="#programs" class="hover:text-vlada-bordo-100 transition-colors font-semibold">Programs</a>
         <a href="#testimonials" class="hover:text-vlada-bordo-100 transition-colors font-semibold">Testimonials</a>
         <a href="#contact" class="hover:text-vlada-bordo-100 transition-colors font-semibold">Contact</a>
